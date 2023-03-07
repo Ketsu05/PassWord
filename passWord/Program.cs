2aa259647c9e193b856f4781f0cee3fed110e624
小杰 陶 <taoyanbojie@outlook.com>
2023-03-07 10:03:26 +0800

    增加了密码长度控制，最小8位最大128位

--- passWord/Program.cs
+++ passWord/Program.cs
@@ -8,41 +8,67 @@ Console.WriteLine("随机生成密码");
 Console.WriteLine("请输入需要生成的密码位数");
 passLen = Console.ReadLine();
 int passwdLen = Convert.ToInt32(passLen);
-while (passwd.Length < passwdLen)
+if (passwdLen >= 8 && passwdLen <= 128)
 {
-    if (passwd.Length % 4 == 0)
+    while (passwd.Length < passwdLen)
     {
-        Random rupC = new Random();
-        char upC = (char)rupC.Next(65, 90);
-        passwd += upC;
+        if (passwd.Length == passwdLen)
+        {
+            break;
+        }
+        else if (passwd.Length % 4 == 0)
+        {
+            Random rupC = new Random();
+            char upC = (char)rupC.Next(65, 90);
+            passwd += upC;
+
+        }
+
+        if (passwd.Length == passwdLen)
+        {
+            break;
+        }
+        else if (passwd.Length % 4 == 1)
+        {
+            Random rlowC = new Random();
+            char lowC = (char)rlowC.Next(97, 122);
+            passwd += lowC;
+        }
+
+        if (passwd.Length == passwdLen)
+        {
+            break;
+        }
+        else if (passwd.Length % 4 == 2)
+        {
+            Random r = new Random();
+            int num = r.Next(10);
+            string numC = num.ToString();
+            passwd += numC;
+        }
+
+        if (passwd.Length == passwdLen)
+        {
+            break;
+        }
+        else if (passwd.Length % 4 == 3)
+        {
+            Random r = new Random();
+            int num = r.Next(10);
+            char spC = spChars[num];
+            passwd += spC;
+        }
+
     }
-
-    if (passwd.Length % 4 == 1)
-    {
-        Random rlowC = new Random();
-        char lowC = (char)rlowC.Next(97, 122);
-        passwd += lowC;
-    }
-
-    if (passwd.Length % 4 == 2)
-    {
-        Random r = new Random();
-        int num = r.Next(10);
-        string numC = num.ToString();
-        passwd += numC;
-    }
-    if (passwd.Length % 4 == 3)
-    {
-        Random r = new Random();
-        int num = r.Next(10);
-        char spC = spChars[num];
-        passwd += spC;
-    }
-
-} 
+    Console.WriteLine("选中并单击右键以复制密码\n" + passwd);
+}
+else
+{
+    Console.WriteLine("密码长度应至少8位，并少于129位");
+}
 
 
-Console.WriteLine("选中并单击右键以复制密码\n" + passwd);
-/*
- 先判断是否第一位，再生成密码；每次循环仅生成一位
+
+/* 
+ * 先判断是否第一位，再生成密码；每次循环仅生成一位
  */
