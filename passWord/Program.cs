3bfe6e5997279134bc206e07e706c3c5f01f0bba
小杰 陶 <taoyanbojie@outlook.com>
2023-03-07 10:03:26 +0800

    实装了自定密码长度功能

--- passWord/Program.cs
+++ passWord/Program.cs
@@ -1,62 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
- 先判断是否第一位，再生成密码；每次循环仅生成一位
- */
-using System;
+﻿using System;
 
-string password = "+";
-int passwdLen = 8;
+
+string passLen = "";
 string passwd = "";
 char[] spChars = { '$', '~', '!', '@', '#','%','^','&','`','-' };
 Console.WriteLine("随机生成密码");
-while (passwdLen == passwd.Length)
+Console.WriteLine("请输入需要生成的密码位数");
+passLen = Console.ReadLine();
+int passwdLen = Convert.ToInt32(passLen);
+while (passwd.Length < passwdLen)
 {
-    if (passwd.Length % 4 == 1)
+    if (passwd.Length % 4 == 0)
     {
         Random rupC = new Random();
         char upC = (char)rupC.Next(65, 90);
-
-    Random rlowC = new Random();
-    char lowC = (char)rlowC.Next(97, 122);
-
-    Random r = new Random();   
-    int num = r.Next(10);
-
-    string numC = num.ToString();
-    char spC = spChars[num];
-    
-    if (password == "+")
-    {
-        passwd = Convert.ToString(upC);
+        passwd += upC;
     }
-    else
+
+    if (passwd.Length % 4 == 1)
     {
-        passwd = string.Concat(upC);
+        Random rlowC = new Random();
+        char lowC = (char)rlowC.Next(97, 122);
+        passwd += lowC;
     }
 
     if (passwd.Length % 4 == 2)
     {
-        Random rlowC = new Random();
-        char lowC = (char)rlowC.Next(97, 122);
-        passwd = string.Concat(lowC);
+        Random r = new Random();
+        int num = r.Next(10);
+        string numC = num.ToString();
+        passwd += numC;
     }
-
     if (passwd.Length % 4 == 3)
     {
         Random r = new Random();
         int num = r.Next(10);
-        string numC = num.ToString();
-        passwd = string.Concat(numC);
-    }
-    if (passwd.Length % 4 == 0)
-    {
-        Random r = new Random();
-        int num = r.Next(10);
         char spC = spChars[num];
-        passwd = string.Concat(spC);
+        passwd += spC;
     }
 
 } 
 
 
 Console.WriteLine("选中并单击右键以复制密码\n" + passwd);
+/*
+ 先判断是否第一位，再生成密码；每次循环仅生成一位
+ */
